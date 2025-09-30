81ea634515c6d8719122c194b24a83cc59add278
tyster <43158716+shadyasdf@users.noreply.github.com>
2025-09-30 01:21:11 -0400

    added select and deselect events to buttons

--- UIK2DButton.cs
+++ UIK2DButton.cs
@@ -1,19 +1,50 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace UIKit
 {
-    public class UIK2DButton : UIK2DSelectable, ISubmitHandler
+    public class UIK2DButton : UIK2DSelectable, ISubmitHandler, ISelectHandler, IDeselectHandler
     {
-        [SerializeField] public UnityEvent<UIKEventData> OnPressed = new();
+        [SerializeField] public UnityEvent<UIKEventData> OnClicked = new();
+        [SerializeField] public UnityEvent<UIKEventData> OnSelected = new();
+        [SerializeField]  public UnityEvent<UIKEventData> OnDeselected = new();
         
 
-        public void OnSubmit(BaseEventData _eventData)
+        protected override void OnPreConstruct(bool _isOnValidate)
+        {
+            base.OnPreConstruct(_isOnValidate);
+            
+            // Clear all OnClick listeners, we do not want to use that event when we're using this UI system
+            Button button = GetComponent<Button>();
+            if (button != null)
+            {
+                button.onClick.RemoveAllListeners();
+            }
+        }
+
+        public virtual void OnSubmit(BaseEventData _eventData)
         {
             if (_eventData is UIKEventData eventData)
             {
-                OnPressed?.Invoke(eventData);
+                OnClicked?.Invoke(eventData);
+            }
+        }
+
+        public void OnSelect(BaseEventData _eventData)
+        {
+            if (_eventData is UIKEventData eventData)
+            {
+                OnSelected?.Invoke(eventData);
+            }
+        }
+
+        public void OnDeselect(BaseEventData _eventData)
+        {
+            if (_eventData is UIKEventData eventData)
+            {
+                OnDeselected?.Invoke(eventData);
             }
         }
     }
--- UIK3DButton.cs
+++ UIK3DButton.cs
@@ -4,16 +4,34 @@ using UnityEngine.EventSystems;
 
 namespace UIKit
 {
-    public class UIK3DButton : UIK3DSelectable, ISubmitHandler
+    public class UIK3DButton : UIK3DSelectable, ISubmitHandler, ISelectHandler, IDeselectHandler
     {
-        [SerializeField] public UnityEvent<UIKEventData> OnPressed = new();
+        [SerializeField] public UnityEvent<UIKEventData> OnClick = new();
+        [SerializeField] public UnityEvent<UIKEventData> OnSelected = new();
+        [SerializeField]  public UnityEvent<UIKEventData> OnDeselected = new();
         
         
         public void OnSubmit(BaseEventData _eventData)
         {
             if (_eventData is UIKEventData eventData)
             {
-                OnPressed?.Invoke(eventData);
+                OnClick?.Invoke(eventData);
+            }
+        }
+
+        public void OnSelect(BaseEventData _eventData)
+        {
+            if (_eventData is UIKEventData eventData)
+            {
+                OnSelected?.Invoke(eventData);
+            }
+        }
+
+        public void OnDeselect(BaseEventData _eventData)
+        {
+            if (_eventData is UIKEventData eventData)
+            {
+                OnDeselected?.Invoke(eventData);
             }
         }
     }
